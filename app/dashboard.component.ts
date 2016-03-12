ddfe6fab2e338f2381d15f0c7f151811df2682f7
Eleanor Demis <eleanordemis@gmail.com>
2016-03-11 17:34:43 -0800

    shows mini nav of selected hero

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from 'angular2/core';
+import { Router } from 'angular2/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+
 
 
 @Component({
@@ -11,7 +13,8 @@ import { HeroService } from './hero.service';
 
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
-  constructor(private _heroService: HeroService) { }
+  constructor(private _router: Router,
+    private _heroService: HeroService) { }
   ngOnInit() {
     this._heroService.getHeroes()
       .then(heroes => this.heroes = heroes.slice(1,5));
